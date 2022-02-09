5a1c51e7121e440c28a64c9cff215b4601946435
kalebscruz <45261702+kalebscruz@users.noreply.github.com>
2022-02-09 14:14:14 -0500

    Update Visualization.py

--- Visualization.py
+++ Visualization.py
@@ -4,4 +4,11 @@ import numpy as np
 theta = np.array(np.arange(0,2 * math.pi, 0.001))
 x = 2 * np.cos(theta) + np.cos(2 * theta)
 y = 2 * np.sin(theta) - np.sin(2 * theta)
-first_example = plt.plot(x, y)
+plt.plot(x, y)
+
+new_theta = np.array(np.arange(0,10 * math.pi, 0.001))
+r=theta*theta
+new_x=r*np.cos(theta)
+new_y=r*np.sin(theta)
+plt.plot(new_x,new_y)
+plt.show()
