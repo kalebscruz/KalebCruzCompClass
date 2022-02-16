7018a41240a83120a09583e18cabc4a73836e835
kalebscruz <45261702+kalebscruz@users.noreply.github.com>
2022-02-16 12:25:40 -0500

    Add files via upload

--- Visualization.py
+++ Visualization.py
@@ -1,14 +1,23 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import math
+import pandas as pd
 
-theta = np.array(np.arange(0,2 * math.pi, 0.001))
-x = 2 * np.cos(theta) + np.cos(2 * theta)
-y = 2 * np.sin(theta) - np.sin(2 * theta)
-plt.plot(x, y)
+part_a_theta = np.array(np.arange(0,2 * math.pi, 0.001))
+part_a_x = 2 * np.cos(part_a_theta) + np.cos(2 * part_a_theta)
+part_a_y = 2 * np.sin(part_a_theta) - np.sin(2 * part_a_theta)
+plt.plot(part_a_x, part_a_y)
 
-new_theta = np.array(np.arange(0,10 * math.pi, 0.001))
-r=theta*theta
-new_x=r*np.cos(theta)
-new_y=r*np.sin(theta)
-plt.plot(new_x,new_y)
-plt.show()
+
+part_b_theta = np.array(np.arange(0,10 * math.pi, 0.001))
+r=part_b_theta*part_b_theta
+part_b_x=r*np.cos(part_b_theta)
+part_b_y=r*np.sin(part_b_theta)
+plt.plot(part_b_x,part_b_y)
+
+def third_part():
+    part_c_theta = np.array(np.arange(0,24*math.pi,0.001))
+    r = pow(10,np.cos(part_c_theta))*2*np.cos(4*part_c_theta)+np.sin(part_c_theta/12)**5
+    part_c_x = r *np.cos(part_c_theta)
+    part_c_y = r * np.sin(part_c_theta)
+    plt.plot(part_c_x, part_c_y)
